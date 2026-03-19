919f6c9a21729bf775e7611bddca56164d415d50
daniordonez11 <arevalodani9@gmail.com>
2026-03-18 20:15:11 -0600

    0.6

--- Backend/routes/v1/index.js
+++ Backend/routes/v1/index.js
@@ -6,20 +6,20 @@ const clientesRoutes = require('./Barberia/cliente.routes');
 const usuariosRoutes = require('./GestionInterna/usuarios.Routes');
 const authRoutes = require("./Auth/auth.routes");
 const dashboardRoutes = require('./GestionInterna/dashboard.routes');
-const { verificarToken, soloAdmin } = require('../../middleware/auth');
+const {  soloAdmin } = require('../../middleware/auth');
 
 const router = Router();
 
 router.use("/auth", authRoutes);
-router.use('/barberos', verificarToken, barberosRoutes);
-router.use('/usuarios', verificarToken, usuariosRoutes);
-router.use('/servicios', verificarToken, serviciosRoutes);
-router.use('/clientes', verificarToken, clientesRoutes);
-router.use('/sucursales', verificarToken, sucursalRoutes);
-router.use('/impuestos', verificarToken, require('./Configuracion/impuesto.routes'));
+router.use('/barberos',  barberosRoutes);
+router.use('/usuarios',  usuariosRoutes);
+router.use('/servicios',  serviciosRoutes);
+router.use('/clientes',  clientesRoutes);
+router.use('/sucursales',  sucursalRoutes);
+router.use('/impuestos',  require('./Configuracion/impuesto.routes'));
 router.use('/dashboard', dashboardRoutes);
-router.use('/citas', verificarToken, require('./Barberia/cita.routes'));
-router.use('/productos',  verificarToken ,require('./Inventario/producto.routes'));
-router.use('/inventario',  verificarToken ,require('./Inventario/inventario.routes'));
+router.use('/citas',  require('./Barberia/cita.routes'));
+router.use('/productos',  require('./Inventario/producto.routes'));
+router.use('/inventario',  require('./Inventario/inventario.routes'));
 
 module.exports = router;
